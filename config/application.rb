27ab0960d170f72cd03408cbdfa04f8800458dc2
tamura <tamura2004@gmal.com>
2016-04-17 12:37:54 +0000

    Remove config of web_console

--- config/application.rb
+++ config/application.rb
@@ -35,8 +35,6 @@ module Session
     config.autoload_paths += Dir[Rails.root.join("app","models","cards")]
     config.autoload_paths += Dir[Rails.root.join("app","models","words")]
 
-    config.web_console.whitelisted_ips = %w(192.168.33.1)
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
